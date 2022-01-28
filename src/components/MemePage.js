e46c850a68fb3ff90e24aa6add9ef4b15dd50c02
Tony Haunschmidt <tmhaunschmidt@gmail.com>
2022-01-28 00:44:13 +0000

    added main functionality of meme page

--- src/components/MemePage.js
+++ src/components/MemePage.js
@@ -1,18 +1,60 @@
-import React from 'react' 
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react' 
+import { useLocation, Link } from 'react-router-dom'
+import axios from 'axios'
+
+//bootstrap
+import Card from 'react-bootstrap/Card'
 
 
 const MemePage = () => {
 
   const location = useLocation()
   const currentMeme = location.state
+  
 
-  console.log(currentMeme)
+  const relatedMemesSampleSize = 10
+  const [ relatedMemesSample, setRelatedMemesSample ] = useState([])
+
+  useEffect(() =>{
+    const getMemes = async() => {
+      try {
+        console.log(currentMeme.subreddit)
+        const { data } = await axios.get(`https://meme-api.herokuapp.com/gimme/${currentMeme.subreddit}/${relatedMemesSampleSize}`)
+        setRelatedMemesSample(data.memes)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getMemes()
+  }, [location])
+
+
+
+
 
   return (
-    <>
-      <img src={currentMeme.preview[0]} alt={currentMeme.title}/>
-    </>
+    <section>
+      { relatedMemesSample.length ?
+        <>
+          {console.log(relatedMemesSample)}
+          <img src={currentMeme.preview[1]} alt={currentMeme.title}/>
+          <div>
+            {relatedMemesSample.map((meme, index) =>
+              <Card key={index}>
+                <Link to={'/meme'} state={{ ...meme }}>
+                  <Card.Img src={meme.preview[1]} />
+                  <Card.Body className='title'>
+                    <Card.Title>{meme.title}</Card.Title>
+                  </Card.Body>
+                </Link>
+              </Card>
+            )}
+          </div>
+        </>
+        :
+        <p>error</p>
+      }
+    </section>
     
   )
 
